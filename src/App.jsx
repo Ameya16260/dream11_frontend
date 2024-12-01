89935a319bf277114905247a84be11bcf8c2af6c
Ameya16260 <ameyalanger2004@gmail.com>
2024-12-02 00:44:41 +0530

    choose

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react';
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Home from './components/home/Home';
-import Upcoming_matches from './components/upcoming_matches/upcoming_matches';
-import Join from './components/join/Join';
-import Choose from './components/join/Choose';
+import { useState } from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./components/home/Home";
+import Upcoming_matches from "./components/upcoming_matches/upcoming_matches";
+import Join from "./components/join/Join";
+import Choose from "./components/join/choose";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -11,11 +11,11 @@ function App() {
   return (
     <div>
       <Router>
-        <Routes> 
+        <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/matches" element={<Upcoming_matches />} />
           <Route path="/join" element={<Join />} />
-          <Route path="/choose" element={<Choose/>} />
+          <Route path="/choose" element={<Choose />} />
         </Routes>
       </Router>
     </div>
